608070eee1c2e957d20d93f2cbe6efbf8840cfb3
Antonio <antonio.rodriguez024@gmail.com>
2021-11-08 22:57:58 -0600

    added breakfast and all filter buttons

--- src/App.js
+++ src/App.js
@@ -7,13 +7,22 @@ function App() {
   const [menuItems, setMenutItems] = useState(items);
   const [categories, setCategories] = useState([]);
 
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenutItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenutItems(newItems)
+  };
+
   return (
     <main>
       <section className='menu section'>
         <div className='title'>
           <h2>our menu</h2>
           <div className='underline'></div>
-          <Categories />
+          <Categories filterItems={filterItems} />
           <Menu items={menuItems} />
         </div>
 
